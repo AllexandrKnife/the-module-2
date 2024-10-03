b97a75d93cb2ce414842f5a5774c1aad6f22b06f
AllexandrKnife <kolchin.de@gmail.com>
2024-10-03 22:13:07 +0300

    Задание #4 Модуль #2
    Дан список чисел numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
    Используя этот список составьте второй список primes содержащий только простые числа.
    А так же третий список not_primes, содержащий все не простые числа.
    Выведите списки primes и not_primes на экран(в консоль).
    Пункты задачи:
    Создайте пустые списки primes и not_primes.
    При помощи цикла for переберите список numbers.
    Напишите ещё один цикл for (вложенный), где будут подбираться делители для числа из 1ого цикла.
    Отметить простоту числа можно переменной is_prime, записав в неё занчение True перед проверкой.
    В процессе проверки на простоту записывайте числа из списка numbers в списки primes и not_primes в зависимости от значения переменной is_prime после проверки (True - в prime, False - в not_prime).
    Выведите списки primes и not_primes на экран(в консоль).
    
    Пример результата выполнения программы:
    Исходный код:
     numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
    Вывод на консоль:
    Primes: [2, 3, 5, 7, 11, 13]
    Not Primes: [4, 6, 8, 9, 10, 12, 14, 15]

---
+++ module_2_4.py
@@ -0,0 +1,22 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+primes = []  # Пустой список простых чисел
+not_primes = []  # Пустой список не простых чисел
+
+for i in numbers:  # 1 не простое и не составное число
+    if i == 1:     # поэтому пропускаем итерацию цикла, если i=1
+        continue
+
+    is_prime = True  # Флаг числа - простое/не простое
+
+    for divisor in range(2, i): 
+        if i % divisor == 0:
+            is_prime = False
+            break  # Выходим из цикла, если найден делитель
+
+    if is_prime:
+        primes.append(i)  # Если флаг числа = истина, то добавляем число в список простых чисел
+    else:
+        not_primes.append(i)  # Иначе, добавляем число в списое не простых чисел
+
+print("Primes:", primes)
+print("Not Primes:", not_primes)
